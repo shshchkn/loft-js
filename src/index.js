17c08045cfbd6ef5fbfc4614c2c441de0db14d3a
shshchkn <shshchkn@yandex.ru>
2017-09-08 01:47:22 +0400

    Update for Homework #5

--- src/index.js
+++ src/index.js
@@ -1,132 +1,69 @@
-/* ДЗ 2 - работа с исключениями и отладчиком */
+/* ДЗ 5 - DOM Events */
 
-/*
- Задача 1:
- Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
- Функция должна вернуть true только если fn вернула true для всех элементов массива
- Необходимо выбрасывать исключение в случаях:
- - array не массив или пустой массив (с текстом "empty array")
- - fn не является функцией (с текстом "fn is not a function")
- Зарпещено использовать встроенные методы для работы с массивами
+/**
+ * Функция должна добавлять обработчик fn события eventName к элементу target
+ *
+ * @param {string} eventName - имя события, на которое нужно добавить обработчик
+ * @param {Element} target - элемент, на который нужно добавить обработчик
+ * @param {function} fn - обработчик
  */
-function isAllTrue(array, fn) {
-    if(array.length === 0 || !(array instanceof Array)) {
-        throw new Error('empty array');
-    }
-    if(typeof fn !== 'function') {
-        throw new Error('fn is not a function');
-    }
-    for(var i =0; i < array.length; i++) {
-        if (fn(array[i] ) === false) {
-            return false;
-        }
-    }
-    return true;
+function addListener(eventName, target, fn) {
 }
 
-/*
- Задача 2:
- Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
- Функция должна вернуть true если fn вернула true хотя бы для одного из элементов массива
- Необходимо выбрасывать исключение в случаях:
- - array не массив или пустой массив (с текстом "empty array")
- - fn не является функцией (с текстом "fn is not a function")
- Зарпещено использовать встроенные методы для работы с массивами
+/**
+ * Функция должна удалять обработчик fn события eventName у элемента target
+ *
+ * @param {string} eventName - имя события, для которого нужно удалить обработчик
+ * @param {Element} target - элемент, у которого нужно удалить обработчик
+ * @param {function} fn - обработчик
  */
-function isSomeTrue(array, fn) {
-    if(array.length === 0 || !(array instanceof Array)) {
-        throw new Error('empty array');
-    }
-    if(typeof fn !== 'function') {
-        throw new Error('fn is not a function');
-    }
-    for(var i =0; i < array.length; i++) {
-        if ( fn(array[i] ) === true) {
-            return true
-        }
-    }
-    return false;
+function removeListener(eventName, target, fn) {
 }
 
-/*
- Задача 3:
- Функция принимает заранее неизветсное количество аргументов, первым из которых является функция fn
- Функция должна поочередно запусти fn для каждого переданного аргумента (кроме самой fn)
- Функция должна вернуть массив аргументов, для которых fn выбросила исключение
- Необходимо выбрасывать исключение в случаях:
- - fn не является функцией (с текстом "fn is not a function")
+/**
+ * Функция должна добавлять к target обработчик события eventName, который должен отменять действие по умолчанию
+ *
+ * @param {string} eventName - имя события, для которого нужно удалить обработчик
+ * @param {Element} target - элемент, на который нужно добавить обработчик
  */
-function returnBadArguments(fn) {
-    var newArr = [];
-    if (typeof fn !== 'function') {
-        throw new Error("fn is not a function");
-    }
-    for(var i = 1; i < arguments.length; i++) {
-        try{
-            fn(arguments[i]);
-        } catch(e) {
-            newArr.push(arguments[i]);
-        }
-    }
-    return newArr;
+function skipDefault(eventName, target) {
 }
 
-/*
- Задача 4:
- Функция имеет параметр number (по умолчанию - 0)
- Функция должна вернуть объект, у которого должно быть несколько методов:
- - sum - складывает number с переданными аргументами
- - dif - вычитает из number переданные аргументы
- - div - делит number на первый аргумент. Результат делится на следующий аргумент (если передан) и так далее
- - mul - умножает number на первый аргумент. Результат умножается на следующий аргумент (если передан) и так далее
-
- Количество передаваемых в методы аргументов заранее неизвестно
- Необходимо выбрасывать исключение в случаях:
- - number не является числом (с текстом "number is not a number")
- - какой-либо из аргументов div является нулем (с текстом "division by 0")
+/**
+ * Функция должна эмулировать событие click для элемента target
+ *
+ * @param {Element} target - элемент, на который нужно добавить обработчик
  */
-function calculator(number) {
-    if(number === undefined) {
-        number = 0;
-    }
-    if(typeof number !== 'number') {
-        throw new Error("number is not a number");
-    }
-    var obj = {
-        sum: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                number += arguments[i];
-            }
-            return number;
-        },
-        dif: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                number -= arguments[i];
-            }
-            return number;
-        },
-        div: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                if(arguments[i] === 0) {
-                    throw new Error("division by 0");
-                }
-                number /= arguments[i];
-            }
-            return number;
-        },
-        mul: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                number *= arguments[i];
-            }
-            return number;
-        }
-    };
-    return obj;
+function emulateClick(target) {
+}
+
+/**
+ * Функция должна добавить такой обработчик кликов к элементу target
+ * который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
+ *
+ * @param {Element} target - элемент, на который нужно добавить обработчик
+ * @param {function} fn - функция, которую нужно вызвать при клике на элемент BUTTON внутри target
+ */
+function delegate(target, fn) {
+}
+
+/**
+ * *** Со звездочкой ***
+ * Функция должна добавить такой обработчик кликов к элементу target
+ * который сработает только один раз и удалится
+ * Постарайтесь не создавать глобальных переменных
+ *
+ * @param {Element} target - элемент, на который нужно добавить обработчик
+ * @param {function} fn - обработчик
+ */
+function once(target, fn) {
 }
 
 export {
-    isAllTrue,
-    isSomeTrue,
-    returnBadArguments,
-    calculator
+    addListener,
+    removeListener,
+    skipDefault,
+    emulateClick,
+    delegate,
+    once
 };
