a983e75d32a01f911ce0b9246b82098db5d8ec8f
shshchkn <shshchkn@yandex.ru>
2017-09-13 00:21:45 +0400

    Homework #6

--- src/index.js
+++ src/index.js
@@ -1,132 +1,25 @@
-/* ДЗ 2 - работа с исключениями и отладчиком */
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
 
-/*
- Задача 1:
- Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
- Функция должна вернуть true только если fn вернула true для всех элементов массива
- Необходимо выбрасывать исключение в случаях:
- - array не массив или пустой массив (с текстом "empty array")
- - fn не является функцией (с текстом "fn is not a function")
- Зарпещено использовать встроенные методы для работы с массивами
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
  */
-function isAllTrue(array, fn) {
-    if(array.length === 0 || !(array instanceof Array)) {
-        throw new Error('empty array');
-    }
-    if(typeof fn !== 'function') {
-        throw new Error('fn is not a function');
-    }
-    for(var i =0; i < array.length; i++) {
-        if (fn(array[i] ) === false) {
-            return false;
-        }
-    }
-    return true;
+function delayPromise(seconds) {
 }
 
-/*
- Задача 2:
- Функция принимает массив и фильтрующую фукнцию и должна вернуть true или false
- Функция должна вернуть true если fn вернула true хотя бы для одного из элементов массива
- Необходимо выбрасывать исключение в случаях:
- - array не массив или пустой массив (с текстом "empty array")
- - fn не является функцией (с текстом "fn is not a function")
- Зарпещено использовать встроенные методы для работы с массивами
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
  */
-function isSomeTrue(array, fn) {
-    if(array.length === 0 || !(array instanceof Array)) {
-        throw new Error('empty array');
-    }
-    if(typeof fn !== 'function') {
-        throw new Error('fn is not a function');
-    }
-    for(var i =0; i < array.length; i++) {
-        if ( fn(array[i] ) === true) {
-            return true
-        }
-    }
-    return false;
-}
-
-/*
- Задача 3:
- Функция принимает заранее неизветсное количество аргументов, первым из которых является функция fn
- Функция должна поочередно запусти fn для каждого переданного аргумента (кроме самой fn)
- Функция должна вернуть массив аргументов, для которых fn выбросила исключение
- Необходимо выбрасывать исключение в случаях:
- - fn не является функцией (с текстом "fn is not a function")
- */
-function returnBadArguments(fn) {
-    var newArr = [];
-    if (typeof fn !== 'function') {
-        throw new Error("fn is not a function");
-    }
-    for(var i = 1; i < arguments.length; i++) {
-        try{
-            fn(arguments[i]);
-        } catch(e) {
-            newArr.push(arguments[i]);
-        }
-    }
-    return newArr;
-}
-
-/*
- Задача 4:
- Функция имеет параметр number (по умолчанию - 0)
- Функция должна вернуть объект, у которого должно быть несколько методов:
- - sum - складывает number с переданными аргументами
- - dif - вычитает из number переданные аргументы
- - div - делит number на первый аргумент. Результат делится на следующий аргумент (если передан) и так далее
- - mul - умножает number на первый аргумент. Результат умножается на следующий аргумент (если передан) и так далее
-
- Количество передаваемых в методы аргументов заранее неизвестно
- Необходимо выбрасывать исключение в случаях:
- - number не является числом (с текстом "number is not a number")
- - какой-либо из аргументов div является нулем (с текстом "division by 0")
- */
-function calculator(number) {
-    if(number === undefined) {
-        number = 0;
-    }
-    if(typeof number !== 'number') {
-        throw new Error("number is not a number");
-    }
-    var obj = {
-        sum: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                number += arguments[i];
-            }
-            return number;
-        },
-        dif: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                number -= arguments[i];
-            }
-            return number;
-        },
-        div: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                if(arguments[i] === 0) {
-                    throw new Error("division by 0");
-                }
-                number /= arguments[i];
-            }
-            return number;
-        },
-        mul: function() {
-            for(var i = 0; i < arguments.length; i++) {
-                number *= arguments[i];
-            }
-            return number;
-        }
-    };
-    return obj;
+function loadAndSortTowns() {
 }
 
 export {
-    isAllTrue,
-    isSomeTrue,
-    returnBadArguments,
-    calculator
+    delayPromise,
+    loadAndSortTowns
 };
